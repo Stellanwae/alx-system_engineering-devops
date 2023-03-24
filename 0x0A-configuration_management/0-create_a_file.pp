c5664029e121debcf6469c1c0e5dbbf18cba206d
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-03-24 15:02:51 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # creating a file using puppet
 
-file { 'school':
-  ensure: 'file',
-  path: '/tmp/school',
-  permission: '0744',
-  owner: 'www-data',
-  group: 'www-data',
-  content: 'I love puppet',
+file { '/tmp/school':
+  content  => 'I love puppet',
+  ensure   => file,
+  mode     => '0744',
+  owner    => www-data,
+  group    => www-data,
 }
