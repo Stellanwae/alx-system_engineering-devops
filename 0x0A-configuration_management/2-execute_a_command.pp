a9a960b215cb384ce3d41ed859e8ebb5e7eb5b92
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-03-24 15:18:31 +0300

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # creat manifest that kills a proceess
 exec { 'killmenow':
-  command  => 'usr/bin/pkill killmenow',
+  command  => '/usr/bin/pkill killmenow',
   provider => 'shell',
+  returns  => [0, 1],
 }
