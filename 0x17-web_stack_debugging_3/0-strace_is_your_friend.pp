853c18ffe28780d350790177c03808a99d0f0153
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-08-09 21:56:05 +0300

    Create 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix the phpp extension in wp-settings
+
+exec { 'fix php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
